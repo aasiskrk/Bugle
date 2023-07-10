ee5f75c0af2c94bdd5050ad6a344f95e8d8f9275
aasiskrk <aasis.krk1@gmail.com>
2023-07-10 07:56:52 +0545

    edited database

--- src/main/java/com/system/bugle/BugleApplication.java
+++ src/main/java/com/system/bugle/BugleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BugleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BugleApplication.class, args);
     }
